0efd5e267ab0dfe12d0994cf415cc7eaa6056319
unknown <Cetecom@SJAPCA03LC0625p.duoc.acad>
2026-04-27 11:38:45 -0400

    modelo creado

--- src/main/java/cl/duoc/La/ReinaBMW/model/Vehiculo.java
+++ src/main/java/cl/duoc/La/ReinaBMW/model/Vehiculo.java
@@ -1,5 +1,22 @@
 package cl.duoc.La.ReinaBMW.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vehiculo {
 
+    @NotBlank(message = "La marca es obligatoria")
+    private String marca;
+    @NotBlank(message = "El modelo es obligatorio")
+    private String modelo;
+    @NotNull(message = "El año es obligatorio")
+    private int año;
+    @NotNull(message = "El precio es obligatorio")
+    private double precio;
+
+   
 }
