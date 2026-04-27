9a0f08d50f63e03ce8fef6d7ecb2539bab600b96
unknown <Cetecom@SJAPCA03LC0631p.duoc.acad>
2026-04-27 11:11:31 -0400

    first commit

---
+++ src/main/java/cl/duoc/La/ReinaBMW/LaReinaBmwApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.La.ReinaBMW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaReinaBmwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaReinaBmwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/La/ReinaBMW/LaReinaBmwApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.La.ReinaBMW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaReinaBmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
